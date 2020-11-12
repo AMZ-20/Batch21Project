bb3ae3255f1b130e12f6754f1caadc0dbf7f6fe6
AMZ-20 <74167413+AMZ-20@users.noreply.github.com>
2020-11-11 23:30:38 -0600

    Commit

--- src/GitHuB/GitHub.java
+++ src/GitHuB/GitHub.java
@@ -2,6 +2,8 @@ package GitHuB;
 
 public class GitHub {
     public static void main(String[] args) {
+        System.out.println("Hello Batch21");
+        System.out.println("this is a stupid class");
 
     }
 }
